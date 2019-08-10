6ee7d63005c80007c419fb5db40df8591edb1424
IlonaArt <ilonasurdream@gmail.com>
2019-08-10 23:35:16 +0300

    Add 1st slider to header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-$(function(){
-
-    $(function(){
-        let $sliderButton = $('.main-header .slider-button');
-        let images = ['url(../img/Img-header1.jpg)', 'url(../img/Img-header2.jpg)', 'url(../img/Img-header3.jpg)', 'url(../img/Img-header4.jpg)'];
-        let currentImage = 0;
-        $sliderButton.on('click', function() {
-            for(let i = 0; i <= images.length; i++)
+(function(){
+    const header = document.querySelector('.main-header');
+    let images = ['url(img/Img-header1.jpg)', 'url(img/Img-header2.jpg)', 'url(img/Img-header3.jpg)', 'url(img/Img-header4.jpg)'];
+    const currentImage = 0;
+    const buttonWrap = header.querySelector('.button-wrap');  
+    for(let i = 0; i < images.length; i++) {
+        buttonWrap.innerHTML += '<button type="button" class="slider-button" aria-label="slider button"></button>';
+    }
+    let sliderButtons = header.querySelectorAll('.slider-button');
+    for(let i = 0; i < sliderButtons.length; i++) {
+        sliderButtons[i].addEventListener('click', function() {
+            header.style.backgroundImage = images[i]; 
         });
-        $('.main-header').css(
-            'background-image': images[currentImage]
-        );
-    });
-});
+    };
+    header.style.backgroundImage = images[currentImage];
+})();
